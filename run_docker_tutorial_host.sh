26cb39c229df5e0efd2508c0331bbc3a9a98ed1c
Vallabh Deogaonkar <63918334+VallabhD@users.noreply.github.com>
2023-02-08 01:31:49 +0530

    Update run_docker_tutorial_host.sh

--- run_docker_tutorial_host.sh
+++ run_docker_tutorial_host.sh
@@ -1,7 +1,9 @@
 docker run -it --rm --device=/dev/ttyUSB0 --name matsya_ros2 \
-    --net=host \
-    --env="DISPLAY" \
-    --workdir="/home/docker/colcon_ws" \
-    --volume=$(pwd)/colcon_ws:"/home/docker/colcon_ws" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    abhilashiit/matsya_ros2:1.0 bash
+ --net=host\
+ --env="DISPLAY"\
+ --env ROS_DOMAIN_ID=79\
+ --workdir="/home/docker/colcon_ws"\
+ --volume=$(pwd)/colcon_ws:"/home/docker/colcon_ws"\
+ --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw"\
+ --volume="/dev/shm:/dev/shm"\
+  abhilashiit/matsya_ros2:1.0 bash
